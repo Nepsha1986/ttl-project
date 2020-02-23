c55743e060728c10a5b301da5cdb79ac0e504125
Alex Nepsha <nepsha1986@gmail.com>
2020-02-24 01:02:56 +0200

    Created ErrorIndicator and ErrorBoundary.

---
+++ src/primitives/error-indicator/ErrorIndicator.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const ErrorIndicator = () => {
+    return (
+        <div>
+            <h3>An error has been occurred. Please come back later</h3>
+        </div>
+    )
+};
