a47e9a22176c5acfb78f8f5008dc5721bfcc3326
Alex Nepsha <nepsha1986@gmail.com>
2020-02-29 00:29:34 +0200

    Added base layout concept.

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -1,25 +1,39 @@
 import React from "react";
 import {
     Switch,
-    Route,
+    Route, Link,
 } from "react-router-dom";
 
 import '../../styles/all.scss';
+import './style.scss';
 
 import {Auth} from "../../pages/auth";
 import {Home} from "../../pages/home";
+import {Header} from "../header/Header";
 
 export const App = () => {
     return (
-        <div>
-            <Switch>
-                <Route exact path="/">
-                    <Home/>
-                </Route>
-                <Route path="/login">
-                    <Auth/>
-                </Route>
-            </Switch>
+        <div className="app">
+            <div className="app__header">
+                <Header/>
+            </div>
+
+            <div className="app__body">
+                <div className="app__aside">
+                    <Link to="/">Home</Link>
+                </div>
+
+                <div className="app__main">
+                    <Switch>
+                        <Route exact path="/">
+                            <Home/>
+                        </Route>
+                        <Route path="/login">
+                            <Auth/>
+                        </Route>
+                    </Switch>
+                </div>
+            </div>
         </div>
     )
 };
--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,13 @@ import reactDOM from 'react-dom';
 
 import {App} from "./components/app/App";
 import ErrorBoundary from "./components/error-boundary";
-import {Header} from "./components/header/Header";
 import {BrowserRouter as Router} from "react-router-dom";
 
 reactDOM.render(
     <ErrorBoundary>
         <Router>
-            <Header/>
             <App/>
         </Router>
     </ErrorBoundary>
-    , document.getElementById('app')
+    , document.getElementById('root')
 );
