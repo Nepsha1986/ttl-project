a057812f995be1081a14f1e5301c3149c19ce246
Alex Nepsha <nepsha1986@gmail.com>
2020-08-02 02:52:22 +0300

    Tiny changes.

--- src/pages/auth/Auth.js
+++ src/pages/auth/Auth.js
@@ -2,10 +2,12 @@ import React from 'react';
 import "./style.scss";
 import {AuthForm} from "../../components/auth-form";
 import {Window} from "../../layouts/Window";
+import {Link} from "react-router-dom";
 
 export const Auth = () => {
     return (
         <Window>
+            <h1 className='mb-3'><Link to={'/'} className={"text-decoration-none"}>Timeline</Link></h1>
             <AuthForm/>
         </Window>
     )
