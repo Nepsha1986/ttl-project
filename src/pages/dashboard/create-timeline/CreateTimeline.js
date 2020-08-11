d8e2bd8218e8a5f54f2ad68b0f903bcd94ed9358
Alex Nepsha <nepsha1986@gmail.com>
2020-08-11 23:45:02 +0300

    Created draft for a item creation form.

--- src/pages/dashboard/create-timeline/CreateTimeline.js
+++ src/pages/dashboard/create-timeline/CreateTimeline.js
@@ -7,56 +7,79 @@ export const CreateTimeline = () => {
         console.log(timeline);
     };
     return (
-        <div>
-            <Formik
-                initialValues={{
-                    timelineTitle: '',
-                    timelineDesc: '',
-                    timelineItems: [],
-                }}
-                onSubmit={(values) => {
-                    createTimeline(values);
-                }}
-                render={({values}) => (
-                    <Form>
-                        <div className="form-group">
-                            <label htmlFor="timelineTitle">Title</label>
-                            <Field type="text" name="timelineTitle"/>
-                        </div>
+        <div className="row">
+            <div className="col-7">
+                <Formik
+                    initialValues={{
+                        timelineTitle: '',
+                        timelineDesc: '',
+                        timelineItems: [],
+                    }}
+                    onSubmit={(values) => {
+                        createTimeline(values);
+                    }}
+                    render={({values}) => (
+                        <Form>
+                            <div className="form-group">
+                                <label htmlFor="timelineTitle">Title</label>
+                                <Field type="text" name="timelineTitle"/>
+                            </div>
 
-                        <div className="form-group">
-                            <label htmlFor="timelineDesc">Description</label>
-                            <Field className="form-control" as="textarea" name={'timelineDesc'}/>
-                        </div>
+                            <div className="form-group">
+                                <label htmlFor="timelineDesc">Description</label>
+                                <Field className="form-control" as="textarea" name={'timelineDesc'}/>
+                            </div>
 
-                        <div className="form-group">
-                            <FieldArray
-                                name="timelineItems"
-                                render={arrayHelpers => (
-                                    <div>
-                                        {values.timelineItems.map((item, index) => (
-                                            <div key={index}>
-                                                <Field name={`timelineItems[${index}].title`}/>
-                                                <Button type="secondary" onClick={() => arrayHelpers.remove(index)}>
-                                                    -
-                                                </Button>
-                                            </div>
-                                        ))}
-                                        <Button
-                                            type="button"
-                                            onClick={() => {arrayHelpers.push({})}}
-                                        >
-                                            +
-                                        </Button>
-                                    </div>
-                                )}
-                            />
-                        </div>
+                            <div className="form-group">
+                                <FieldArray
+                                    name="timelineItems"
+                                    render={arrayHelpers => (
+                                        <div>
+                                            {values.timelineItems.map((item, index) => (
+                                                <div key={item.title}>
+                                                    <div className="item-form">
+                                                        <div className="item-form__header">
+                                                            <h3>TimeLine item</h3>
+                                                            <Button type="secondary" onClick={() => arrayHelpers.remove(index)}>
+                                                                <i className="fas fa-trash" />
+                                                            </Button>
+                                                        </div>
 
-                        <Button type={'submit'}>Submit</Button>
-                    </Form>
-                )}
-            />
+                                                        <div className="item-form__body">
+                                                            <div className="form-group">
+                                                                <Field placeholder="Title" name={`timelineItems[${index}].title`}/>
+                                                            </div>
+                                                            <div className="form-group">
+                                                                <Field placeholder="Year" name={`timelineItems[${index}].year`}/>
+                                                            </div>
+                                                            <div className="form-group">
+                                                                <Field
+                                                                    className="form-control"
+                                                                    placeholder="Timeline item information"
+                                                                    as="textarea"
+                                                                    name={`timelineItems[${index}].event`}
+                                                                />
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            ))}
+                                            <Button
+                                                type="button"
+                                                onClick={() => {arrayHelpers.push({})}}
+                                            >
+                                                Add new item
+                                            </Button>
+                                        </div>
+                                    )}
+                                />
+                            </div>
+
+                            <Button type={'submit'}>Submit</Button>
+                        </Form>
+                    )}
+                />
+            </div>
         </div>
     )
 };
